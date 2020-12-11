bb098042971b0781e2cc95fc41f286e91a7425d1
J. Sterling Bishop <jsterlingbishop@gmail.com>
2020-12-11 12:34:39 -0800

    added form

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -20,11 +21,32 @@ db.create_all()
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 toolbar = DebugToolbarExtension(app)
 
+
 @app.route('/')
 def display_pets_list():
-    """ displays up to date list of pets """
+    """ Displays up to date list of pets. """
 
     pets = Pet.query.all()
 
     return render_template('pet_listing.html',
                            pets=pets)
+
+
+@app.route('/add', methods=["GET", "POST"])
+def display_add_pet_form():
+    """ Displays form to add a new pet. """
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        pet = Pet(
+            name=form.name.data,
+            species=form.species.data,
+            photo_url=form.photo_url.data,
+            age=form.age.data,
+            notes=form.notes.data)
+        db.session.add(pet)
+        db.session.commit()
+        flash(f"Added new pet: {pet.name}")
+        return redirect("/add")
+    else:
+        render_template("add_pet.html", form=form)
--- forms.py
+++ forms.py
@@ -1,4 +1,20 @@
 """Forms for adopt app."""
-# from flask_wtf import FlaskForm
-# from wtforms import StringField, FloatField
-# from wtforms.validators import InputRequired, Optional, Email
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, SelectField
+from wtforms.validators import InputRequired, Optional, Email, URL
+
+
+class AddPetForm(FlaskForm):
+    """ Form to add a pet. """
+    name = StringField("Name",
+                       validators=[InputRequired()])
+    species = StringField("Species",
+                          validators=[InputRequired()])
+    photo_url = StringField("Photo URL",
+                            validators=[Optional(), URL()])
+    age = SelectField("Age",
+                      choices=[('baby', 'young', 'adult', 'senior'),
+                               ('Baby', 'Young', 'Adult', 'Senior')],
+                      validators=[InputRequired()])
+    notes = StringField("Notes",
+                        validators=[Optional()])
